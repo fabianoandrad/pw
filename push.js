c1162e507b9c766affe1c00384705dce7c7bc6f0
fabiano_andrade <fabiano.andrade@zurg.com.br>
2022-11-01 12:46:26 -0300

    fix

--- push.js
+++ push.js
@@ -7,9 +7,6 @@ let vapidKeys = {
 
   push.setVapidDetails('mailto:test@code.com.br', vapidKeys.publicKey, vapidKeys.privateKey)
 
-  let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/d1CRjudWRjA:APA91bEVNQb7ILMzdpkfKwPFKNj9lz4qCFpJS1TvUcvP0CdbhzGZgn_qk22xjp-zcxA4SlJefjNdeZ5ynU7pBNI_I44IGbvwD3VLqisugwoSJhVe69uPKDeQU4HHu3tyw2Ifgt7FcTu7",
-  "expirationTime":null,
-  "keys":{"p256dh":"BC6YWoBtw9F2g4cDhr78Lrp3_C7jFBScTXz61MQd4mvBvQcIATHU6MZ1Q3ccNVq2EjLpj3aSuBM2_wl1vg6K5ew",
-  "auth":"TMpce1VrSXzs3AbdjdQABQ"}};
-  
+  let sub = {"endpoint":"https://wns2-bl2p.notify.windows.com/w/?token=BQYAAACL9EcS1KpDl%2f1bfkVNZjOI5kKVFRaKuuZ1nEBL3V82iUpRBm6tONczqbLCeLKooTAmxzx9Q2kmLSbAuw%2b6AgRnWTjZsF%2bQNxvmvBV%2bGa4X9V5P%2bGNbdURPqPC62D6JganYXlhisgyWczViy9bApOuLnMQlHGRjHR2THiIGrkj7TOzRvLl%2bu4RC649ZuFx2WTX9%2b83BcXj7D0dYjtBO4VQV8ZwTFgh2RB6N2ZoEpYn5f0pSikLM9tzj9xL78VjLbo0Hqi2avSdcYkChMHOz%2fK%2f6vMLGu9rMepNuHlUqsld5kjyElELBTW3fgGS4NrlCc5kQSHBBBF9QWzDQ3uTXdKXX","expirationTime":null,"keys":{"p256dh":"BITB5Gc2xopB2Gis2GKndBLdo-MWqhSGHVeYccaXEmBHbsoZHrKcCewQNHDMK9ByJjwKCNOkvtpFbSoLo_-IbXA","auth":"abSgwsgUnan9-bw2l1F5OA"}}
+
   push.sendNotification(sub, 'Teste notificação')
