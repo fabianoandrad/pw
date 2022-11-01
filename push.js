de7684fe7c3f6573c84f3c2151c1c96a14d554f4
fabiano_andrade <fabiano.andrade@zurg.com.br>
2022-11-01 12:22:56 -0300

    fix

--- push.js
+++ push.js
@@ -7,5 +7,9 @@ let vapidKeys = {
 
   push.setVapidDetails('mailto:test@code.com.br', vapidKeys.publicKey, vapidKeys.privateKey)
 
-  let sub = {}
+  let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/d1CRjudWRjA:APA91bEVNQb7ILMzdpkfKwPFKNj9lz4qCFpJS1TvUcvP0CdbhzGZgn_qk22xjp-zcxA4SlJefjNdeZ5ynU7pBNI_I44IGbvwD3VLqisugwoSJhVe69uPKDeQU4HHu3tyw2Ifgt7FcTu7",
+  "expirationTime":null,
+  "keys":{"p256dh":"BC6YWoBtw9F2g4cDhr78Lrp3_C7jFBScTXz61MQd4mvBvQcIATHU6MZ1Q3ccNVq2EjLpj3aSuBM2_wl1vg6K5ew",
+  "auth":"TMpce1VrSXzs3AbdjdQABQ"}};
+  
   push.sendNotification(sub, 'Teste notificação')
